89eeb3b35b5d9b6c81c3d4b9d3ead91338bdfd01
ARJYOUU03 <arjunaravind145@gmail.com>
2024-02-08 15:32:51 +0530

    okkkk

--- exam/urls.py
+++ exam/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='hello'),
-    path('exam',views.ExamView.as_view(),name='exam'),
+    path('exam/',views.ExamView.as_view(),name='exam'),
     path('timetable',views.ExamTimeTableView.as_view(),name='timetable'),
     path('duty',views.dutyAllotmentView.as_view(),name='dutyAllotment'),
     path('prefer',views.preferTableView.as_view(),name='preferTable')
